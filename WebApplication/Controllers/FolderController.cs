6ff1fa52ee91ae0dd9fce0dfc47a19cf11031aa6
Unknown <Mikolaj.Slon@wp.pl>
2017-11-17 19:45:50 +0100

    Added some more advanced actions and custom routes

--- WebApplication/Controllers/FolderController.cs
+++ WebApplication/Controllers/FolderController.cs
@@ -15,5 +15,26 @@ namespace WebApplication.Controllers
             var folder = new Folder("summer", 1,1);
             return View(folder);
         }
+
+        public ActionResult AddFolder(int DeviceId)
+        {
+            return Content(DeviceId.ToString());
+        }
+
+        //string is nullable so parameter is not necessary
+        public ActionResult Folders(string sortBy)
+        {
+            if(String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "decreasing";
+            }
+            return Content(String.Format("sort by = {0}", sortBy));
+        }
+
+        [Route("folder/BindDevice/{Folderid:min(0)}/{DeviceId:min(0)}")]
+        public ActionResult BindDevice(int FolderId, int DeviceId)
+        {
+            return Content("fodler id = " + FolderId + " device ID " + DeviceId);
+        }
     }
 }
