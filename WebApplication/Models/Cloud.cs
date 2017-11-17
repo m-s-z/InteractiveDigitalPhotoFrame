feca28f1e277dc793a10e4bd2874b95e53434495
Unknown <Mikolaj.Slon@wp.pl>
2017-11-17 21:51:53 +0100

    Added clouds
    Added controller, model, viewmodel and html file

---
+++ WebApplication/Models/Cloud.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public enum ProviderType
+    {
+        GoogleDrive, OneDrive, DropBox, Flicker
+    }
+    public class Cloud
+    {
+        public Cloud(ProviderType provider, string login)
+        {
+            Provider = provider;
+            Login = login;
+        }
+
+        public ProviderType Provider { get; set; }
+        public string Login { get; set; }
+    }
+}
---
+++ WebApplication/ViewModels/CloudViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Models;
+
+namespace WebApplication.ViewModels
+{
+    public class CloudViewModel
+    {
+        public CloudViewModel(List<Cloud> clouds)
+        {
+            Clouds = clouds;
+        }
+
+        public List<Cloud> Clouds { get; set; }
+    }
+}
