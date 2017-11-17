287d20c6808636960811bb3357aad2d60c86516b
Unknown <Mikolaj.Slon@wp.pl>
2017-11-17 22:08:59 +0100

    Added Account Tab
    added all necessary files for the account tab

---
+++ WebApplication/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Account
+    {
+        public Account(string accountName, string password = "")
+        {
+            AccountName = accountName;
+            Password = password;
+        }
+
+        public String AccountName { get; set; }
+        public String Password { get; set; }
+    }
+}
---
+++ WebApplication/ViewModels/AccountViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Models;
+
+namespace WebApplication.ViewModels
+{
+    public class AccountViewModel
+    {
+        public AccountViewModel(Account account)
+        {
+            Account = account;
+        }
+
+        public Account Account { get; set; }
+    }
+}
