fd5dc5507043b7714f62637ed585922ed651989c
_msz_ <em.eszet5@gmail.com>
2017-11-17 09:50:13 +0100

    Comments and architecture proposal

---
+++ AAA/AAA/ViewModels/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿namespace AAA.ViewModels
+{
+    /// <summary>
+    /// MainViewModel class.
+    /// Provides properites and methods to handle application logic.
+    /// </summary>
+    public class MainViewModel : IDPFLibrary.ViewModelBase
+    {
+        #region fields
+
+        /// <summary>
+        /// Path to some files
+        /// </summary>
+        private const string EXAMPLE_OF_CONST = "path/to/some/files";
+
+        /// <summary>
+        /// Backing field for TestNumberOne property.
+        /// </summary>
+        private string _testNumberOne;
+
+        /// <summary>
+        /// Backing field for TestNumberTwo property.
+        /// </summary>
+        private bool _testNumberTwo;
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Property indicating some testing value.
+        /// </summary>
+        public string TestNumberOne
+        {
+            get => _testNumberOne;
+
+            set => SetProperty(ref _testNumberOne, value);
+        }
+
+        /// <summary>
+        /// Flag indicating whether is something or not.
+        /// </summary>
+        public bool TestNumberTwo
+        {
+            get => _testNumberTwo;
+
+            set => SetProperty(ref _testNumberTwo, value);
+        }
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// TestMethod method.
+        /// Updates value of TestNumberOne property if secondParameter is true.
+        /// </summary>
+        /// <param name="firstParameter">Value to assing to TestNumberOne property.</param>
+        /// <param name="secondParameter">Flag indicating whether to change value of TestNumberOne property or not.</param>
+        /// <returns>Returns flag indicating whether the value of TestNumberOne property was changed or not.</returns>
+        private bool TestMethod(string firstParameter, bool secondParameter)
+        {
+            if (secondParameter)
+            {
+                TestNumberOne = firstParameter;
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+    }
+}
---
+++ IDPFLibrary/ViewModelBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDPFLibrary
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region properties
+
+        /// <summary>
+        /// PropertyChanged event handler.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region methods
+
+        /// <summary>
+        /// Upadates value of the "storage" argument with value given by the second argument.
+        /// Notifies the application about update of the property which has executed this method.
+        /// </summary>
+        /// <typeparam name="T">Property type.</typeparam>
+        /// <param name="storage">Storage field.</param>
+        /// <param name="value">New value of the property.</param>
+        /// <param name="propertyName">Property name.</param>
+        /// <returns>Returns true if storage is successfully updated, false otherwise.</returns>
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        /// <summary>
+        /// Notifies the application about update of the property with name given as a parameter.
+        /// </summary>
+        /// <param name="propertyName">Property name.</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
