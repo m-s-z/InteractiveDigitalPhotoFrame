41bf4e903734f359cf0965b94e4d6171387ec502
Unknown <Mikolaj.Slon@wp.pl>
2017-11-17 19:15:09 +0100

    Added Folder controller and page

---
+++ WebApplication/Controllers/FolderController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class FolderController : Controller
+    {
+        // GET: Folder
+        public ActionResult Index()
+        {
+            var folder = new Folder("summer", 1,1);
+            return View(folder);
+        }
+    }
+}
---
+++ WebApplication/Models/Folder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Folder
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DeviceId { get; set; }
+
+        public Folder(string Name, int Id, int Device)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.DeviceId = Device;
+        }
+    }
+}
