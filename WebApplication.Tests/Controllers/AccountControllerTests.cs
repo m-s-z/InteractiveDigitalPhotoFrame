0a3cd906997232138cfc2bb89f4e5cffea20d081
Unknown <Mikolaj.Slon@wp.pl>
2017-11-24 03:06:24 +0100

    first test

---
+++ WebApplication.Tests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers.Tests
+{
+    [TestClass()]
+    public class AccountControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            AccountController controller = new AccountController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
