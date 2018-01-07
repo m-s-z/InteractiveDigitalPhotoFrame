c9e66cf694bc13a7f1d6841c4a782c1fbeef9084
Unknown <Mikolaj.Slon@wp.pl>
2018-01-07 22:58:33 +0100

    Home controller comments

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -6,14 +6,25 @@ using System.Web.Mvc;
 
 namespace WebApplication.Controllers
 {
+    /// <summary>
+    /// Controller responsible for the home page
+    /// </summary>
     public class HomeController : Controller
     {
+        #region methods
         //should not be authenticated since this runs before we can redirect to login
+        /// <summary>
+        /// displays the home page
+        /// </summary>
+        /// <returns>
+        /// Home view
+        /// </returns>
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
         }
+        #endregion methods
     }
 }
