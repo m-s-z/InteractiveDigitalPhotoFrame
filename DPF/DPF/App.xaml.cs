35830c15900ed215ecd1b5f7ff5c40ac0cb99c30
_msz_ <em.eszet5@gmail.com>
2018-01-07 23:31:06 +0100

    App class comments

--- DPF/DPF/App.xaml.cs
+++ DPF/DPF/App.xaml.cs
@@ -1,19 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using DPF.Models;
+﻿using DPF.Models;
 using DPF.Views;
 using Xamarin.Forms;
 
 namespace DPF
 {
+    /// <summary>
+    /// Main application class.
+    /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Application class constructor.
+        /// Checks whether the device token has already been saved in the storage or not.
+        /// If yes - shows main page, otherwise - presents welcome page.
+        /// </summary>
         public App()
         {
             InitializeComponent();
             var deviceToken = DependencyService.Get<ILocalStorageService>().GetDeviceToken();
+
             if (deviceToken == null)
             {
                 MainPage = new WelcomePage();
@@ -22,21 +27,6 @@ namespace DPF
             {
                 MainPage = new NavigationPage(new MainAppPage());
             }
-        }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
         }
     }
 }
