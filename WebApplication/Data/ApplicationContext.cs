831c39d54672abc193cb1bdf9da422fd83a6591f
Unknown <Mikolaj.Slon@wp.pl>
2018-01-08 02:43:42 +0100

    dbContext Comments

--- WebApplication/Data/ApplicationContext.cs
+++ WebApplication/Data/ApplicationContext.cs
@@ -7,17 +7,44 @@ using WebApplication.Models;
 
 namespace WebApplication.Data
 {
+    /// <summary>
+    /// Application context class, represents the database. Based on this class the entity framework generates a database.
+    /// </summary>
     public class ApplicationContext : DbContext
     {
+        #region properties
+        /// <summary>
+        /// devices
+        /// </summary>
         public DbSet<Device> Devices { get; set; }
+        /// <summary>
+        /// accounts
+        /// </summary>
         public DbSet<Account> Accounts { get; set; }
+        /// <summary>
+        /// clouds
+        /// </summary>
         public DbSet<Cloud> Clouds { get; set; }
+        /// <summary>
+        /// folders
+        /// </summary>
         public DbSet<Folder> Folders { get; set; }
+        /// <summary>
+        /// DeviceNames
+        /// </summary>
         public DbSet<DeviceName> DeviceNames { get; set; }
+        #endregion properties
+        #region methods
+
+        /// <summary>
+        /// overriden onModelCreating method with our Application context
+        /// </summary>
+        /// <param name="modelBuilder">model builder</param>
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             Database.SetInitializer<ApplicationContext>(null);
             base.OnModelCreating(modelBuilder);
         }
+        #endregion methods
     }
 }
